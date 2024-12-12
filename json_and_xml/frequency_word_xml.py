af34857cf7d9500e0c330bc0ec51baafc5a3c9cf
Роман Данилов <danroman@rambler.ru>
2024-12-12 22:24:20 +0300

    create folder json_and_xml

---
+++ json_and_xml/frequency_word_xml.py
@@ -0,0 +1,44 @@
+import xml.etree.ElementTree as ET
+
+# Функция составления списка слов
+def fetch_words(news_list):
+    words = []
+
+    for line in news_list:
+        text = line.find('description').text
+        word_list = text.lower().split()
+
+        for word in word_list:
+
+            if len(word) > 6:
+                words.append(word)
+
+    return words
+
+
+# Функция подсчета частоты слов
+def count_words(words):
+    counts_word = {}
+
+    for word in words:
+
+        if word in counts_word:
+            counts_word[word] += 1
+        else:
+            counts_word[word] = 1
+
+    return counts_word
+
+
+parser = ET.XMLParser(encoding='utf-8')
+tree = ET.parse('newsafr.xml', parser)
+root = tree.getroot()
+news_list = root.findall('channel/item')
+words = fetch_words(news_list)
+counts_word = count_words(words)
+top_10 = sorted(counts_word.items(), key=lambda x: x[1], reverse=True)[:10]
+
+print('Топ 10 самых часто встречающихся в новостях слов,\n'
+      'в файле newsafr.xml, длиннее 6 символов:')
+for word, count in top_10:
+    print(f'{word} - {count}')
