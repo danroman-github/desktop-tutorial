44491a1c0c93313721b6374891105530241aa73a
Роман Данилов <danroman@rambler.ru>
2024-12-13 15:29:34 +0300

    update

--- json_and_xml/frequency_word_json.py
+++ json_and_xml/frequency_word_json.py
@@ -1,14 +1,19 @@
+import os
 import json
 
-# Функция получения данных
-def open_news(open_file):
 
-    with open(open_file, encoding='utf-8') as f:
+def open_news(file_name):
+    """Функция получения данных"""
+    if not os.path.exists(file_name):
+        print(f"Файл {file_name} не найден. Пропускаем.")
+        return
+
+    with open(file_name, encoding='utf-8') as f:
         return json.load(f)
 
 
-# Функция составления списка слов
 def fetch_words(news_list):
+    """Функция составления списка слов"""
     words = []
 
     for line in news_list:
@@ -16,34 +21,30 @@ def fetch_words(news_list):
         word_list = text.split()
 
         for word in word_list:
-
             if len(word) > 6:
                 words.append(word)
-
     return words
 
 
-# Функция подсчета частоты слов
 def count_words(words):
+    """Функция подсчета частоты слов"""
     counts_word = {}
 
     for word in words:
-
         if word in counts_word:
             counts_word[word] += 1
         else:
             counts_word[word] = 1
-
     return counts_word
 
 
-news_data = open_news('newsafr.json')
+file_name = 'newsafr.json'
+news_data = open_news(file_name)
 news_list = news_data['rss']['channel']['items']
 words = fetch_words(news_list)
 counts_word = count_words(words)
 top_10 = sorted(counts_word.items(), key=lambda x: x[1], reverse=True)[:10]
 
-print('Топ 10 самых часто встречающихся в новостях слов,\n'
-      'в файле newsafr.json, длиннее 6 символов:')
+print(f'Топ 10 самых часто встречающихся в новостях слов,\nв файле {file_name}, длиннее 6 символов:')
 for word, count in top_10:
     print(f'{word} - {count}')
--- json_and_xml/frequency_word_xml.py
+++ json_and_xml/frequency_word_xml.py
@@ -1,44 +1,48 @@
 import xml.etree.ElementTree as ET
 
-# Функция составления списка слов
+
 def fetch_words(news_list):
+    """Функция составления списка слов"""
     words = []
 
     for line in news_list:
-        text = line.find('description').text
-        word_list = text.split()
+        if hasattr(line, 'find'):
+            description_element = line.find('description')
+            if description_element is not None and description_element.text:
+                text = description_element.text
+                word_list = text.split()
 
-        for word in word_list:
-
-            if len(word) > 6:
-                words.append(word)
-
+                for word in word_list:
+                    if len(word) > 6:
+                        words.append(word)
+            else:
+                print("Предупреждение: элемент 'description' отсутствует или пуст.")
+        else:
+            print("Ошибка: элемент не имеет метода 'find'.")
     return words
 
 
-# Функция подсчета частоты слов
 def count_words(words):
+    """Функция подсчета частоты встречаемости слов"""
     counts_word = {}
 
     for word in words:
-
         if word in counts_word:
             counts_word[word] += 1
         else:
             counts_word[word] = 1
-
     return counts_word
 
 
+file_name = 'newsafr.xml'
 parser = ET.XMLParser(encoding='utf-8')
-tree = ET.parse('newsafr.xml', parser)
+tree = ET.parse(file_name, parser)
 root = tree.getroot()
 news_list = root.findall('channel/item')
 words = fetch_words(news_list)
 counts_word = count_words(words)
 top_10 = sorted(counts_word.items(), key=lambda x: x[1], reverse=True)[:10]
 
-print('Топ 10 самых часто встречающихся в новостях слов,\n'
-      'в файле newsafr.xml, длиннее 6 символов:')
+print(f'Топ 10 самых часто встречающихся в новостях слов,\nв файле {file_name}, длиннее 6 символов:')
 for word, count in top_10:
     print(f'{word} - {count}')
